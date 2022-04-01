836740aa857a48bb4fee354f55ac72cb82bddcb9
punitdiwan <rohitkumar44jhansi@gmail.com>
2022-04-01 14:05:58 +0530

    permision added

--- Airpmo-bakend/src/seeders/role.seeder.ts
+++ Airpmo-bakend/src/seeders/role.seeder.ts
@@ -1,23 +1,37 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Seeder} from "nestjs-seeder";
-import { Role, RoleDocument } from "src/schemas/roles.schema";
-import { users, ussersDocument } from "src/schemas/users.schema";
-import { UserRole, UserRoleDocument } from "src/schemas/user_roles.schema";
- 
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Seeder } from 'nestjs-seeder';
+import { Role, RoleDocument } from 'src/schemas/roles.schema';
+import { users, ussersDocument } from 'src/schemas/users.schema';
+import { UserRole, UserRoleDocument } from 'src/schemas/user_roles.schema';
+
 @Injectable()
 export class RolesSeeder implements Seeder {
-  constructor(@InjectModel(Role.name) private RoleModel: Model<RoleDocument>,@InjectModel(UserRole.name) private UserRoleModel: Model<UserRoleDocument>,@InjectModel(users.name) private usersModel: Model<ussersDocument>) {}
- 
+  constructor(
+    @InjectModel(Role.name) private RoleModel: Model<RoleDocument>,
+    @InjectModel(UserRole.name) private UserRoleModel: Model<UserRoleDocument>,
+    @InjectModel(users.name) private usersModel: Model<ussersDocument>,
+  ) {}
+
   async seed(): Promise<any> {
+    let role = await this.RoleModel.insertMany([
+      { name: 'Airpmo Super Admin', permission: 'ALL' },
+      { name: 'super admin', permission: 'ALL' },
+      { name: 'riyaz', permission: 'ALL' },
+      { name: 'neeraj', permission: 'ALL' },
+      { name: 'aditya', permission: 'ALL' },
+    ]);
+    let user = await this.usersModel.find();
 
-    let role= await this.RoleModel.insertMany([{name:"Airpmo Super Admin",permission:'ALL'},{name:"super admin",permission:'ALL'}]);
-    let user= await this.usersModel.find();
-    return await this.UserRoleModel.insertMany({role_id:role[0].id,user_id:user[user.length-1].id});
-
+    for (let i = 0; i < user.length; i++) {
+      var userid = await this.UserRoleModel.insertMany({
+        role_id: role[i].id,
+        user_id: user[i]._id,
+      });
+    }
   }
- 
+
   async drop(): Promise<any> {
     return this.RoleModel.deleteMany({});
   }
