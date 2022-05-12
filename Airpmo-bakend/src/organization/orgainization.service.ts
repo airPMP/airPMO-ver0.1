a51d88ec19fd47e52df49fa7ccd88805c3f31332
punitdiwan <rohitkumar44jhansi@gmail.com>
2022-05-12 18:07:42 +0530

    organization url for admin it is work

--- Airpmo-bakend/src/organization/orgainization.service.ts
+++ Airpmo-bakend/src/organization/orgainization.service.ts
@@ -2,9 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
-import {
-  orgainization,
-  orgainizationDocument,
+import {orgainization,orgainizationDocument,
 } from 'src/schemas/organization.schema';
 import { users, ussersDocument } from 'src/schemas/users.schema';
 import { UsersService } from 'src/users/users.service';
